f48f8ff9f6ac93866716bb50fd76003c3935bc53
changhyeon <changhyeon.h@kakao.com>
2023-12-02 21:30:34 +0900

    build: init project

---
+++ src/main/java/com/pickple/batch/PickpleBackBatchApplication.java
@@ -0,0 +1,12 @@
+package com.pickple.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpleBackBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickpleBackBatchApplication.class, args);
+    }
+}
